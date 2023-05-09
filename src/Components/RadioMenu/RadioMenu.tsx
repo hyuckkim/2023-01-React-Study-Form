c5b580ca01463b986f96864eaabffddf76c98a42
hyuckkim <monkeyhyuck@naver.com>
2023-05-09 20:14:03 +0900

    remove unused variable radio

--- src/Components/RadioMenu/RadioMenu.tsx
+++ src/Components/RadioMenu/RadioMenu.tsx
@@ -11,19 +11,19 @@ function RadioMenu(prop: RadioMenuProps) {
     const reset = () => {
         setSelected(null);
     }
-    const select = (s: string) => () => {
+    const select = (s: string) => () => { //TODO
         setSelected(s);
     }
     
-    const radio = prop.items.map (e => 
-        <div key={e} className={styles.element}>
-            <input type="radio" id={e} className={styles.input} name={prop.summary} checked={select != null && selected === e} 
-            onClick={select(e)}/>
-            <label className={styles.inputlabel} htmlFor={e}>{e}</label>
-        </div>)
     return <form className={styles.root}>
         {prop.summary && <div>{prop.summary}</div>}
-        {radio}
+        {prop.items.map (e => 
+        <div key={e} className={styles.element}>
+            <input type="radio" id={e} className={styles.input} name={prop.summary} 
+            checked={selected != null && selected === e} 
+            onClick={select(e)}/>
+            <label className={styles.inputlabel} htmlFor={e}>{e}</label>
+        </div>)}
         <div className={styles.removeselect} onClick={reset}>선택해제</div>
     </form>
 }
